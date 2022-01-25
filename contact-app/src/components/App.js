bcbafe39c08a0793ebce79259029e44ec3ae99e2
Amrut Sagar <amrutsagar06@gmail.com>
2022-01-25 23:29:07 +0530

    reactb

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -5,11 +5,26 @@ import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+
+  const contacts = [
+    {
+      id : "1",
+      name : "Amrutsagar",
+      email : "amrutsagar06@gmail.com"
+    },
+    {
+      id : "2",
+      name : "Harigovind",
+      email : "harigovind97@gmail.com"
+    }
+  ];
   return (
    
-    <div>
+    <div className="ui container">
+<Header />
+<AddContact />
+<ContactList contacts={contacts} />
 
-      
     </div>     //JSX  (combination of javscript and html)
 
   );
