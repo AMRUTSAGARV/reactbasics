d1bca8c825a0595bdabccb92a40e0a1f52303d0f
Amrut Sagar <amrutsagar06@gmail.com>
2022-03-25 00:30:08 +0530

    html,css,javascript

---
+++ fePortfolio/app.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controlls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function PageTransitions() {
+  //Button Click active class.
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", () => {
+      let currentBtn = document.querySelectorAll(".");
+    });
+  }
+}
