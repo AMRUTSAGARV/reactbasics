e6a337b191b407f8e10826b7ba425426f622c6dd
Amrut Sagar <amrutsagar06@gmail.com>
2022-04-03 11:45:32 +0530

    navbar

--- admindashboard/src/components/navbar/Navbar.jsx
+++ admindashboard/src/components/navbar/Navbar.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './navbar.scss'
-
+import ChatBubbleOutlineRoundedIcon from '@mui/icons-material/ChatBubbleOutlineRounded';
+import NotificationsNoneRoundedIcon from '@mui/icons-material/NotificationsNoneRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import LanguageRoundedIcon from '@mui/icons-material/LanguageRounded';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import ViewListRoundedIcon from '@mui/icons-material/ViewListRounded';
+import FullscreenExitRoundedIcon from '@mui/icons-material/FullscreenExitRounded';
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className='wrapper'>
+      <div className="search">
+        <input type="text" placeholder='Search...' />
+        <SearchRoundedIcon/>
+      </div>
+      <div className="items">
+        <div className="item">
+          <LanguageRoundedIcon
+          className='icon'/>English
+        </div>
+        <div className="item">
+          <DarkModeRoundedIcon className='icon'/>
+        </div>
+        <div className="item">
+          <FullscreenExitRoundedIcon className='icon'/>
+        </div>
+        <div className="item">
+          <NotificationsNoneRoundedIcon className='icon'/>
+          <div className='counter'>1</div>
+        </div>
+        <div className="item">
+        <div className='counter'>2</div>
+          <ChatBubbleOutlineRoundedIcon  className='icon'/>
+        </div>
+        <div className="item">
+          <ViewListRoundedIcon className='icon'/>
+        </div>
+        <div className='item'>
+          <img 
+          src='https://thumbs.dreamstime.com/b/portrait-young-man-beard-hair-style-male-avatar-vector-portrait-young-man-beard-hair-style-male-avatar-105082137.jpg' 
+          alt='' 
+          className='avatar'
+          />
+        </div>
+       
+      </div>
+      </div>
+      </div>
   )
 }
 
--- admindashboard/src/pages/home/Home.jsx
+++ admindashboard/src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import './home.scss';
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className="home">
 
         <Sidebar />
-        <div className='homeContainer'>container</div>
+        <div className="homeContainer">
+          <Navbar/>
+          home container</div>
     </div>
   )
 }
