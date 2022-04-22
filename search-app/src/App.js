f43432da5dcd571672a96c35c0b00002922c84ca
Amrut Sagar <amrutsagar06@gmail.com>
2022-04-22 23:16:22 +0530

    table

---
+++ search-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import BasicTable from "./components/SearchFilter/SearchFilter";
+// import BasicTable from "./components/SearchFilter/SearchFilter";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <BasicTable />
+    </div>
+  );
+}
+
+export default App;
